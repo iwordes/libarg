da71d3929af99fbb5666fd43cefb139836f6b15a
Ian Wordes <iwordes@student.42.us.org>
2017-04-08 19:28:19 -0700

    State commit

--- libarg.h
+++ libarg.h
@@ -6,19 +6,16 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/08 09:53:15 by iwordes           #+#    #+#             */
-/*   Updated: 2017/04/08 15:48:01 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/04/08 19:28:11 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBARG_H
 # define LIBARG_H
 
-#include <stdbool.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#define ARG_LONG 1
-#define ARG_SHOR 0
+# include <stdbool.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 #define T_ARG0 void (*)()
 #define T_ARG1 void (*)(char*)
